cfffed768de111fb6ff70b1880bac5266d9cba52
Vaibhav Kumar <vaibjais123456@gmail.com>
2025-06-25 05:15:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.9 MB (77.43%)

---
+++ 3421-count-pairs-that-form-a-complete-day-i/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        vector<int>a(24,0);
+        for(int i:hours){
+            a[i%24]++;
+        }
+        long long int count=0;
+        count+=1LL*a[0]*(a[0]-1)/2;
+        for(int i=1;i<=24/2;++i){
+            if(i!=24-i){
+                count+=a[i]*a[24-i];
+            }
+            else{
+                count+=a[i]*(a[i]-1)/2;
+                }
+
+
+        }
+        return count;
+        
+    }
+};
