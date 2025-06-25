62804dcddb8a1e55531bc9730da4168a8ceebb4d
Vaibhav Kumar <vaibjais123456@gmail.com>
2025-06-25 10:01:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.8 MB (82.87%)

---
+++ 2102-find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n=nums.size();
+        int total=0;
+        for(int i:nums){
+            total+=i;
+        }
+        int ltotal=0;
+        for(int i=0;i<n;i++){
+            if(ltotal==total-ltotal-nums[i]){
+                return i;
+            };
+            ltotal+=nums[i];
+        }
+        return -1;
+    }
+};
