13001468f28b8e3cd08ad79557f820618a92f15e
Vaibhav Kumar <vaibjais123456@gmail.com>
2025-06-26 04:31:52 +0000

    [LeetCode Sync] Runtime - 4 ms (66.45%), Memory - 98 MB (98.91%)

---
+++ 1783-ways-to-make-a-fair-array/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int n = nums.size();
+        int totalEven = 0, totalOdd = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) totalEven += nums[i];
+            else totalOdd += nums[i];
+        }
+
+        int count = 0;
+        int leftEven = 0, leftOdd = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) totalEven -= nums[i];
+            else totalOdd -= nums[i];
+            if (leftEven + totalOdd == leftOdd + totalEven)
+                count++;
+            if (i % 2 == 0) leftEven += nums[i];
+            else leftOdd += nums[i];
+        }
+
+        return count;
+        
+    }
+};
