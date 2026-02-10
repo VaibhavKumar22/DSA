24d10f4a5309a16c71cdb38b8fedc729777b6afa
Vaibhav Kumar <vaibjais123456@gmail.com>
2026-02-10 08:36:54 +0000

    [LeetCode Sync] Runtime - 7 ms (21.08%), Memory - 8.9 MB (42.91%)

--- 0326-power-of-three/solution.cpp
+++ 0326-power-of-three/solution.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1){
-            return true;
-        }
-        while(n>1){
-            if(n%3==0){
-                n=n/3;
-            }
-            else{
-                break;
-            }
-        }
-        if(n==1){
-            return true;
-        }
-        
-            return false;
-        }
-
-        
+        if(n==0)return false;
+        while(n%3==0)n/=3;
+        return n==1;
       
+    }
 };
