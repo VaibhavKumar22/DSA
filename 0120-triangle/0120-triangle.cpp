09a8abb533a890d6be22c26e56dc0959e4c6592d
Vaibhav Kumar <vaibjais123456@gmail.com>
2026-07-18 20:59:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (97.62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,29 @@
 class Solution {
-    vector<vector<int>>dp;
-    int dfs(vector<vector<int>>& triangle,int row,int i){
-        if(triangle.size()==row)return 0;
-        if(dp[row][i]!=INT_MAX)return dp[row][i];
-        int case1=dfs(triangle,row+1,i);
-        int case2=dfs(triangle,row+1,i+1);
-        return dp[row][i]=min(case1,case2)+triangle[row][i];
-}
+//     vector<vector<int>>dp;
+//     int dfs(vector<vector<int>>& triangle,int row,int i){
+//         if(triangle.size()==row)return 0;
+//         if(dp[row][i]!=INT_MAX)return dp[row][i];
+//         int case1=dfs(triangle,row+1,i);
+//         int case2=dfs(triangle,row+1,i+1);
+//         return dp[row][i]=min(case1,case2)+triangle[row][i];
+// }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int row=0;
-        int i=0;
-        dp.resize(triangle.size(),vector<int>(triangle.size(),INT_MAX));
-        return dfs(triangle,row,i);
+        // int row=0;
+        // int i=0;
+        // dp.resize(triangle.size(),vector<int>(triangle.size(),INT_MAX));
+        // return dfs(triangle,row,i);
 
         //without dp we can do it by modifying the origanal traiangle
-        // int n=triangle.size(); 
-        // for(int i=n-2;n>=0;i--){
-        //     for(int j=0;j<triangle[i].size();j++){
-        //         int child1=triangle[i+1][j];
-        //         int child2=triangle[i+1][j+1];
-        //         triangle[i][j]+=min(child1,child2);
-        //     }
-        // }    
-        // return triangle[0][0];   
+        int n=triangle.size(); 
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                int child1=triangle[i+1][j];
+                int child2=triangle[i+1][j+1];
+                triangle[i][j]+=min(child1,child2);
+            }
+        }    
+        return triangle[0][0];   
     }
 
 
